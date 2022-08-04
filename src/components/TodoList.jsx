161c0db170990d38dae56bee6ae7fc288ffe401c
nimi2838 <bbongsky11@naver.com>
2022-08-04 15:05:53 +0900

    backup

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import TodoListItem from "./TodoListItem";
+import "../styles/TodoList.scss";
+
+const TodoList = ({ todos, onToggle, onRemove, onInsertToggle, setSelectedTodo }) => {
+  return (
+    <ul className="TodoList">
+      {todos.map((todo, index) => (
+        <TodoListItem todo={todo} 
+        key={index} 
+        onToggle={onToggle} 
+        onRemove={onRemove}
+        onInsertToggle= {onInsertToggle}
+        setSelectedTodo = {setSelectedTodo} />
+      ))}
+    </ul>
+  );
+};
+
+export default TodoList;
